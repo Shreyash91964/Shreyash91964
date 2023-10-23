375aa2b1e5c7750dbbb6d68373db1fa6df756124
unknown <shreyashkumar1008@gmail.com>
2023-10-24 04:26:15 +0530

    first commit

---
+++ Landlord Tenent management System/database/landlordtenentmanagementsystem.sql
@@ -0,0 +1,32 @@
+{
+    "Servers": {
+        "1": {
+            "Name": "PostgreSQL 16",
+            "Group": "Servers",
+            "Host": "localhost",
+            "Port": 5432,
+            "MaintenanceDB": "postgres",
+            "Username": "postgres",
+            "Comment": "Auto-detected PostgreSQL 16 installation with the data directory at C:\\Program Files\\PostgreSQL\\16\\data",
+            "KerberosAuthentication": false,
+            "ConnectionParameters": {
+                "sslmode": "prefer",
+                "connect_timeout": 10
+            }
+        },
+        "2": {
+            "Name": "PostgreSQL 13",
+            "Group": "Servers",
+            "Host": "localhost",
+            "Port": 5432,
+            "MaintenanceDB": "postgres",
+            "Username": "postgres",
+            "Comment": "Auto-detected PostgreSQL 13 installation with the data directory at C:\\Program Files\\PostgreSQL\\13\\data",
+            "KerberosAuthentication": false,
+            "ConnectionParameters": {
+                "sslmode": "prefer",
+                "connect_timeout": 10
+            }
+        }
+    }
+}
